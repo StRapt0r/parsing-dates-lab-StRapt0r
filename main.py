3f6cb0d85bfdf9e45a5f60d6e627aafafefce5a0
lmraine <55120024+lmraine@users.noreply.github.com>
2024-02-01 09:46:53 -0600

    updated instructions

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #parse month should take in the text of the month and return the number 
 #as a string
 #January -> 1 (as a string)
+#YOU MAY USE THIS FUNCTION IF YOU WANT TO OR YOU MAY REMOVE IT
 def parse_month(month):
     pass
 
+#REMOVE PASS AND FIX THIS FUNCTION
 #parse_date function should return the date formated as MM/DD/YYYY
+#DO NOT REMOVE THIS FUNCTION
 def parse_date(user_string):
     pass
 
