ce983f4bb8d337bc64e466da9b08c51eb4ff16c4
Elijah <87155048+StRapt0r@users.noreply.github.com>
2024-02-17 15:27:03 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 #January -> 1 (as a string)
 #YOU MAY USE THIS FUNCTION IF YOU WANT TO OR YOU MAY REMOVE IT
 def parse_month(month):
-    pass
+    months={"January":"01","February":"02","March":"03","April":"04","May":"05","June":"06","July":"07","August":"08","September":"09","October":"10","November":"11","December":"12",}
+    for i in months:
+        if month==months[i]:
+            return(months[i])
 
 #REMOVE PASS AND FIX THIS FUNCTION
 #parse_date function should return the date formated as MM/DD/YYYY
