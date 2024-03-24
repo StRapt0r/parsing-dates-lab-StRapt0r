6c8444de95bd5a628af4b630f40dee1382517ca4
Elijah <87155048+StRapt0r@users.noreply.github.com>
2024-03-24 15:24:51 -0700

    finaly have time

--- main.py
+++ main.py
@@ -13,32 +13,24 @@ def parse_month(month):
 #parse_date function should return the date formated as MM/DD/YYYY
 #DO NOT REMOVE THIS FUNCTION
 def parse_date(user_string):
-    bannana=user_string.split()
-    x=0
-    retdate=""
-    for i in bannana:
-        x+=1
-        if x==1:
-            month=parse_month(i)
-        elif x==2:
-            i=i.replace(",","")
-            i=i.replace(" ","")
-            if len(i) < 2:
-                i="0"+i
-            day=i
-        elif x==3:
-            i=i.replace(" ","")
-            year=i
-    return(str(month) + "/" str(day) + "/" str(year))
+    x = x.split(user_string)
+    for i in x:
+        x[i].replace(" ", "")
+        x[i].replace(",", "")
+    x[0]=parse_month(x[0])
+    for i in range(2):
+        x[i].append("/")
+    x=(str(y) for y in x)
+    return(x)
 
 #March 1, 1990
 #03/01/1990
 #REMOVE PASS AND YOUR CODE GOES HERE
 if __name__ == '__main__':
-    data=[]
-    z=input()
-    while -1 != z:
-        data.append(z)
-        z=input()
-    for y in data:
-        print(parse_date(y))
+    h=0
+    while h==0:
+        inp=input()
+        if inp== -1 or "-1":
+            h=1
+        else:
+            print(parse_date(inp))
