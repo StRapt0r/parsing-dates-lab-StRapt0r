1b50df96c88e378ebe75bca695111f5c5452f0c1
Elijah <87155048+StRapt0r@users.noreply.github.com>
2024-03-25 08:20:30 -0700

    Finally works as intended

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
-#parse month should take in the text of the month and return the number 
-#as a string
-#January -> 1 (as a string)
-#YOU MAY USE THIS FUNCTION IF YOU WANT TO OR YOU MAY REMOVE IT
+
 def parse_month(month):
-    months={"January":"01","February":"02","March":"03","April":"04","May":"05","June":"06",
-            "July":"07","August":"08","September":"09","October":"10","November":"11","December":"12",}
-    for i in months:
-        if month==months[i]:
-            return(months[i])
+    months={"January":"01", "February":"02", "March":"03", "April":"04", "May":"05", "June":"06", 
+            "July":"07", "August":"08", "September":"09", "October":"10", "November":"11", "December":"12"}
+    if month in months:
+        return months[month]
 
-#REMOVE PASS AND FIX THIS FUNCTION
-#parse_date function should return the date formated as MM/DD/YYYY
-#DO NOT REMOVE THIS FUNCTION
+
 def parse_date(user_string):
-    x = x.split(user_string)
-    for i in x:
-        x[i].replace(" ", "")
-        x[i].replace(",", "")
-    x[0]=parse_month(x[0])
-    for i in range(2):
-        x[i].append("/")
-    x=(str(y) for y in x)
-    return(x)
+    x = user_string.split()
+    day = str(x[1][:-1])
+    month = str(parse_month(x[0]))
+    year = str(x[2])
+    ret = month + "/" + day + "/" + year
+    return(ret)
 
-#March 1, 1990
-#03/01/1990
-#REMOVE PASS AND YOUR CODE GOES HERE
+
 if __name__ == '__main__':
     h=0
     while h==0:
-        inp=input()
-        if inp== -1 or "-1":
+        inp=str(input())
+        if inp== "-1":
             h=1
         else:
             print(parse_date(inp))
